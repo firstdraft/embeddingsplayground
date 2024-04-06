f4336270aa5808e9ffaf9a4959e320a40d071404
Raghu Betina <raghubetina@users.noreply.github.com>
2024-04-05 20:01:04 -0400

    Configure generators

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,11 @@ module Embeddingsplayground
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    
+      config.generators do |g|
+    g.system_tests = nil
+    g.scaffold_stylesheet false
+  end
+
   end
 end
